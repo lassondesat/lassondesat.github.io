8d7d836ef4ad6d975a4072a353497f12cc096ff9
milkal <lijiammilka@gmail.com>
2018-11-19 00:11:38 -0500

    major changes
    completely rebuilt the website

--- js/components/wow.js
+++ js/components/wow.js
@@ -1,25 +1,22 @@
 // Wow
 var Wow = function() {
-    "use strict";
+  'use strict';
 
-    // Handle Wow
-    var handleWow = function() {
-        var wow = new WOW({
-		    boxClass:     'wow',      // animated element css class (default is wow)
-		    offset:       0,          // distance to the element when triggering the animation (default is 0)
-		    mobile:       false,      // trigger animations on mobile devices (true is default)
-		    tablet:       false       // trigger animations on tablet devices (true is default)
-		});
-		wow.init();
-    }
+  // Handle Wow
+  var handleWow = function() {
+    var wow = new WOW({
+      mobile: false
+  	});
+  	wow.init();
+  }
 
-    return {
-        init: function() {
-            handleWow(); // initial setup for counter
-        }
+  return {
+    init: function() {
+      handleWow(); // initial setup for Wow
     }
+  }
 }();
 
 $(document).ready(function() {
-    Wow.init();
+  Wow.init();
 });
